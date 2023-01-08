5e7c25ac17d237a481d85849721a9da1613f2603
John <doingeometry@gmail.com>
2023-01-08 23:28:17 +0200

    aaandagain

--- src/app-index.ts
+++ src/app-index.ts
@@ -99,7 +99,7 @@ export class AppIndex extends LitElement {
             path: '(.*)',
             component: 'app-404',
             redirect: '/404',
-            response: 404,
+            statusCode: 404,
             action: async () => {
               await import('./pages/app-404.js');
             },
