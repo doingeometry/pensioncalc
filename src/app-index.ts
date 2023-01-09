fc250de305e7d433e33c949b1b96afb3a654ffbd
John <doingeometry@gmail.com>
2023-01-09 13:39:54 +0200

    remove redirect on router (404)

--- src/app-index.ts
+++ src/app-index.ts
@@ -98,11 +98,10 @@ export class AppIndex extends LitElement {
           {
             path: '(.*)',
             component: 'app-404',
-            redirect: '/404',
             action: async () => {
               await import('./pages/app-404.js');
             },
-            statusCode: 404 //TODO: still doesn't work!
+            statusCode: 404
           },
         ],
       } as any,
